27a2681801adf243c7f367353b37eb7b7e2f6179
LSL Web Development <lslwebdevelopment@gmail.com>
2025-06-13 11:41:49 -0300

    Added some style to the accordion component

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -22,25 +22,37 @@ export function Accordion({ accordionData }: AccordionProps) {
   const renderedItems = accordionData.map(
     (item: AccordionEntity, index: number) => {
       const showItem = index === isOpened;
-      const icon = showItem ? (
-        <GoChevronRight className="text-4xl" />
-      ) : (
-        <GoChevronDown className="text-4xl font-extrabold" />
+      const icon = (
+        <span>
+          {showItem ? (
+            <GoChevronRight className="text-4xl" />
+          ) : (
+            <GoChevronDown className="text-4xl font-extrabold" />
+          )}
+        </span>
       );
       return (
         <div key={item.accordionId}>
           <div
-            className="flex justify-between"
+            className="flex p-3 bg-gray-300 border-b items-center justify-between cursos-pointer"
             onClick={() => handleOpenCloseAccordion(index)}
           >
             {item.accordionTitle}
             {icon}
           </div>
-          {showItem && <div>{item.accordionDescription}</div>}
+          {showItem && (
+            <div className="borde-b p-5 bg-amber-200">
+              {item.accordionDescription}
+            </div>
+          )}
         </div>
       );
     }
   );
 
-  return <div>{renderedItems}</div>;
+  return (
+    <div className="w-[40rem] my-10 m-auto border-x border-5 rouded">
+      {renderedItems}
+    </div>
+  );
 }
